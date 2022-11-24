b6f683c5694fe42ffc9792c5d1a21ade788c760e
Darshan singh <88918534+imdarshansingh@users.noreply.github.com>
2022-11-24 16:19:12 +0530

    Time: 7 ms (14.89%), Space: 6.1 MB (23.46%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+//         double count =1;
+//         double ans;
+//         if(n==0)
+//             return count ;
+//         if(n<0){
+//             int z= abs(n);
+//              double q = pow(x, z);
+//              ans =1/q ;
+//         }
+//           else{
+//                ans = pow(x, n);  
+//           } 
+                 
+        
+//         return ans;
+//     }
+        
+        return (double) pow(x,n);
+    }
+};
