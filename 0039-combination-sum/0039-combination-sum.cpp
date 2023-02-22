d42b67fae8f7116576baea69298fca3d1b31f937
Darshan singh <88918534+imdarshansingh@users.noreply.github.com>
2023-02-22 11:13:05 +0530

    Time: 11 ms (48.27%), Space: 10.8 MB (82.62%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,25 +1,24 @@
 class Solution {
-    void combination(vector<int>& candidates, int target, vector<int> currComb, int currSum, int currIndex, vector<vector<int>>& ans){
-        if(currSum>target) return; //backtrack
-        if(currSum==target){
-            ans.push_back(currComb); //store the solution and backtrack
-            return;
+void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> &ans,vector<int>&ds){
+    if(ind == arr.size()){
+        if(target == 0){
+            ans.push_back(ds);
         }
-        
-        for(int i=currIndex; i<candidates.size(); i++){ //try all possible options for the next level
-            currComb.push_back(candidates[i]); //put 1 option into the combination
-            currSum+=candidates[i];
-            combination(candidates, target, currComb, currSum, i, ans); //try with this combination, whether it gives a solution or not.
-            currComb.pop_back(); //when this option backtrack to here, remove this and go on to the next option.
-            currSum-=candidates[i];
-        }
-        
+        return;
     }
+    if(arr[ind] <= target){
+        ds.push_back(arr[ind]);
+        findCombination(ind,target-arr[ind] , arr, ans,ds);
+        ds.pop_back();
+    }
+    
+    findCombination(ind+1 , target,arr,ans,ds);
+}
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
-        vector<int> currComb;
-        combination(candidates, target, currComb, 0, 0, ans);
+        vector<int> ds;
+        findCombination(0 , target,candidates, ans, ds);
         return ans;
     }
 };
