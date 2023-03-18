52ef42182e63b0fbbdb3f7cc00dc5825215b02cd
Darshan singh <88918534+imdarshansingh@users.noreply.github.com>
2023-03-18 16:10:29 +0530

    Time: 235 ms (15.63%), Space: 122.2 MB (9.17%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.cpp
@@ -0,0 +1,42 @@
+class BrowserHistory {
+
+public:
+    stack<string> history;
+    stack<string> future;
+	
+    BrowserHistory(string homepage) {
+        history.push(homepage);
+        future = stack<string>(); // Reset as can't go forward now
+    }
+    
+    void visit(string url) {
+        history.push(url);
+        future = stack<string>(); // Reset as can't go forward now
+    }
+    
+    string back(int steps) {
+        while(steps > 0 && history.size() > 1) { // As we can only move x steps & stesp>x then we return only x steps
+            future.push(history.top());
+            history.pop();
+            steps--;
+        }
+        return history.top();
+    }
+    
+    string forward(int steps) {
+        while(steps > 0 && future.size() > 0) {
+            history.push(future.top());
+            future.pop();
+            steps--;
+        }
+        return history.top(); 
+    }
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */
