fbe6e91ccea1d65870c46cc2608a690c49c32805
Darshan singh <88918534+imdarshansingh@users.noreply.github.com>
2023-04-10 22:33:01 +0530

    Time: 3 ms (45.98%), Space: 5.9 MB (57.18%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -11,5 +11,20 @@ public:
             else lo = mid+1;
         }
         return lo;
+//         int mid ;
+//         vector<int> v;
+//         for(int i =1;i<n ;i++){
+//             v.push_back(i);
+//         }
+//         if(n%2 ==0){
+//             mid = n/2;
+//         }
+//         else mid =n/2 +1;
+        
+//         if(mid %2==0){
+//         return mid;
+//         }
+//         else return mid+1;
+            
     }
 };
