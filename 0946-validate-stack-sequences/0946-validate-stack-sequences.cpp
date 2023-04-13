435577f02d3170c77b4236313aea0d3a81dff680
Darshan singh <88918534+imdarshansingh@users.noreply.github.com>
2023-04-13 10:36:00 +0530

    Time: 14 ms (25.17%), Space: 15.5 MB (24.28%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-//     stack<int> ans;
-//         int j=0;
-//         for(int i=0;i<pushed.size();++i){
-//         ans.push(pushed[i]);
+    stack<int> ans;
+        int j=0;
+        for(int i=0;i<pushed.size();++i){
+        ans.push(pushed[i]);
             
-//             while( !ans.empty() && ans.top() == popped[j]){
-//                 ans.pop();
-//              //  i++; 
-//                 ++j;
-//             }
+            while( !ans.empty() && ans.top() == popped[j]){
+                ans.pop();
+             //  i++; 
+                ++j;
+            }
             
-//     } 
+    } 
 //         if(ans.empty()){
 //             return "true";
 
 //         }
 //         else
 //             return "false";
-           stack<int> s ;   // an empty stack
-        int j = 0;
-        for(int i= 0; i<pushed.size(); ++i)
-        {
-            s.push(pushed[i]);
+        return ans.empty();
+//            stack<int> s ;   // an empty stack
+//         int j = 0;
+//         for(int i= 0; i<pushed.size(); ++i)
+//         {
+//             s.push(pushed[i]);
             
-			// check if pushed values is next to pe popped out.
-            while(!s.empty() && s.top() == popped[j])
-            {
-                s.pop();
-                ++j;
-            }
-        }
-		// if stack is empty means sequence is correct.
-        return s.empty();
+// 			// check if pushed values is next to pe popped out.
+//             while(!s.empty() && s.top() == popped[j])
+//             {
+//                 s.pop();
+//                 ++j;
+//             }
+//         }
+// 		// if stack is empty means sequence is correct.
+//         return s.empty();
     }
     
 };
