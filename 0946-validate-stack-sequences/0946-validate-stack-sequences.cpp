b440a00275761efc2bbb4643f459a8eb9ef89823
Darshan singh <88918534+imdarshansingh@users.noreply.github.com>
2023-04-13 10:34:25 +0530

    Time: 18 ms (9.35%), Space: 15.2 MB (98.01%) - LeetHub

---
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+//     stack<int> ans;
+//         int j=0;
+//         for(int i=0;i<pushed.size();++i){
+//         ans.push(pushed[i]);
+            
+//             while( !ans.empty() && ans.top() == popped[j]){
+//                 ans.pop();
+//              //  i++; 
+//                 ++j;
+//             }
+            
+//     } 
+//         if(ans.empty()){
+//             return "true";
+
+//         }
+//         else
+//             return "false";
+           stack<int> s ;   // an empty stack
+        int j = 0;
+        for(int i= 0; i<pushed.size(); ++i)
+        {
+            s.push(pushed[i]);
+            
+			// check if pushed values is next to pe popped out.
+            while(!s.empty() && s.top() == popped[j])
+            {
+                s.pop();
+                ++j;
+            }
+        }
+		// if stack is empty means sequence is correct.
+        return s.empty();
+    }
+    
+};
