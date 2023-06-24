e564708dca65085899b103718a80ff26bb024594
Darshan singh <88918534+imdarshansingh@users.noreply.github.com>
2023-06-24 13:54:42 +0530

    Time: 4 ms (47.66%), Space: 5.8 MB (93.74%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.cpp
@@ -0,0 +1,12 @@
+#include<vector>
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(i%3==0 || i%5 ==0 || i%7==0){
+                count += i;} 
+        }
+        return count;
+    }
+};
