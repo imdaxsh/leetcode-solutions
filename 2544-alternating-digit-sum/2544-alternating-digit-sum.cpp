432959da5b7931cc7f88abce668ceecfdc637449
Darshan singh <88918534+imdarshansingh@users.noreply.github.com>
2023-06-30 18:42:14 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (96.90%) - LeetHub

--- 2544-alternating-digit-sum/2544-alternating-digit-sum.cpp
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
     int alternateDigitSum(int n) {
-     // string s = to_string(n);
-     //    int count =0;
-     //    for(int i=0;i<=s.size();i++){
-     //        if(i%2==0){
-     //            count+=s[i];
-     //        }else{ count -= s[i];}
-     //    }
-     //    return count;
-        
-         int sum = 0;
-        bool flag = true;
-        // convert the number to a string so that we can
-        // iterate through the string from both the sides
-        string num = to_string(n);
-        for(int i=0; i<num.size(); i++) {
-            // subtract the characters (ASCII values)
-            // and obtain the integer number 
-            if(flag) {
-                sum = sum + (num[i] - '0');
+     string s = to_string(n);
+        int count =0;
+        bool flag=true;
+        for(int i=0;i<s.size();i++){
+            if(flag){
+                count+=(s[i]- '0');
                 flag = false;
-            } else {
-                sum = sum - (num[i] - '0');
-                flag = true;
-            }
-            // int flag = 1;
-            // flag = 1 - flag;
+            }else{ count -= (s[i]- '0');
+                 flag=true;
+                 }
         }
-        return sum;
+        return count;
     }
 };
