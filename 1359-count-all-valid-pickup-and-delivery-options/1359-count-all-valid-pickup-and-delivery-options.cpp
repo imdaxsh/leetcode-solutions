000e503d9fb8206c2f09abdb8a8d57210499e190
Darshan singh <88918534+imdarshansingh@users.noreply.github.com>
2023-09-10 17:28:49 +0530

    Time: 0 ms (100.00%), Space: 6 MB (43.33%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    
+   int mod=1e9+7;
+
+   int countOrders(int n) {
+        long long ans = 1;
+        for (int i = 1; i <= n; i++) {
+            // Ways to arrange all pickups, 1*2*3*4*5*...*n
+            ans = (ans * i)%mod;
+            // Ways to arrange all deliveries, 1*3*5*...*(2n-1)
+            ans = (ans * (2 * i - 1))%mod;
+        }
+        return ans;
+    }
+};
