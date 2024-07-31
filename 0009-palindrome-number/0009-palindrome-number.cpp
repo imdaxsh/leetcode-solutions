7c70baf8923b457730199e7032b5f87e768b5d05
Darshan singh <88918534+imdarshansingh@users.noreply.github.com>
2024-07-31 09:59:00 +0530

    Time: 7 ms (71.65%), Space: 8.6 MB (30.57%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    bool isPalindrome(int x) {
+//        long r=0;
+//        long z=x;
+//         if(x<0) {x*=-1;}
+        
+        
+//         while(x){
+//              r=r*10 +x%10;
+//              x=x/10;
+//         }
+//        return z==r;
+          long t=0;
+        long v=x;
+        if(v<0) v*=-1; 
+        while(x){
+            t=t*10 + (x%10);
+            x/=10;
+        }
+         cout<< 3%10<<"\n"<< -3%10; 
+        return t==v;
+    }
+};
