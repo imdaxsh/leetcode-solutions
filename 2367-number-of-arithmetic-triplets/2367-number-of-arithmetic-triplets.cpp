117d5dc3cf454a298680e2d1d81c26fae1e8f8b3
Darshan singh <88918534+imdarshansingh@users.noreply.github.com>
2024-08-01 11:46:06 +0530

    Time: 26 ms (15.14%), Space: 10.8 MB (55.89%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -4,8 +4,8 @@ public:
         int n= nums.size();
         int count(0);
         for(int i=0;i<n-2;i++){
-            for(int j=0;j<n-1;j++){
-                for(int k=0;k<n;k++){
+            for(int j=i+1;j<n-1;j++){
+                for(int k=j+1;k<n;k++){
                    if((nums[k]-nums[j]== diff) && (nums[j]-nums[i]==diff))  count++;
                 }
             }
